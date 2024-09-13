d882ec589206a777ed026f1c402055d6fa48a3aa
Niklas <niklasberby@gmail.com>
2024-09-13 08:36:02 +0200

    Refactor backend code to add CORS middleware and set tab size to 4

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,56 +1,56 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { serveStatic } from "@hono/node-server/serve-static";
+import { cors } from "hono/cors"
 
 import projects from "./projects.json";
 
 // Define Project interface
 interface Project {
-  id: number,
-  name: string,
-  description: string,
-  technologies: string[]
+	id: number,
+	name: string,
+	description: string,
+	technologies: string[]
 };
 
 const app = new Hono();
 
-// Serve static files from './src/public'
-app.use('/*', serveStatic({ root: './src/public' }));
+// cors middleware to allow cross-origin requests
+app.use("/*", cors());
 
 // GET endpoint to retrieve all projects
 app.get("/api/projects", (c) => c.json(projects));
 
 // POST endpoint to add a new project
 app.post('/api/projects', async (c) => {
-  const body = await c.req.json();
+	const body = await c.req.json();
 
-  // Parse technologies string into array
-  const technologies = body.technologies ? (body.technologies as string).split(',').map(tech => tech.trim()) : [];
+	// Parse technologies string into array
+	const technologies = body.technologies ? (body.technologies as string).split(',').map(tech => tech.trim()) : [];
 
-  // Create new project object
-  const project: Project = {
-    id: projects.length + 1,
-    name: body.name as string,
-    description: body.description as string,
-    technologies
-  };
+	// Create new project object
+	const project: Project = {
+		id: projects.length + 1,
+		name: body.name as string,
+		description: body.description as string,
+		technologies
+	};
 
-  projects.push(project);
-  console.log('Project added:', project);
-  
- 
-  
-  // Return response
-  // 201 CREATED
-  c.status(201);
-  // TODO: Add error handling
-  return c.json({message: 'Project added successfully', project: body});
+	projects.push(project);
+	console.log('Project added:', project);
+
+
+
+	// Return response
+	// 201 CREATED
+	c.status(201);
+	// TODO: Add error handling
+	return c.json({ message: 'Project added successfully', project: body });
 });
 
 // Start server
 const port = 3000;
 console.log(`Server is running on port ${port}`);
 serve({
-  fetch: app.fetch,
-  port
+	fetch: app.fetch,
+	port
 });
