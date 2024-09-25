bee14e4c82697349aa1f9b56f433b78f9ab96848
Niklas <niklasberby@gmail.com>
2024-09-25 21:26:09 +0200

    Update ProjectList to display text when the list of projects is empty

--- frontend/src/components/ProjectList.tsx
+++ frontend/src/components/ProjectList.tsx
@@ -7,6 +7,21 @@ import { Project, ApiResponsePOST } from "@shared/types";
 import { ProjectFormData } from './ProjectListForm';
 
 
+const ProjectFlexbox = ({ projects, onDelete }: { projects: Project[], onDelete: (id: string) => void }) => {
+    if (projects.length === 0) {
+        return <p className="flex-grow text-lg text-gray-50">No projects found</p>;
+    }
+
+    return (
+        <div className="flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {projects.map(project => (
+                <ProjectBox key={project.id} project={project} onDelete={onDelete} />
+            ))}
+        </div>
+
+    )
+};
+
 const ProjectList = () => {
     const [projects, setProjects] = useState<Project[]>([]);
 
@@ -74,14 +89,10 @@ const ProjectList = () => {
         <div className="container mx-auto px-4 py-12 rounded-lg shadow-lg ">
             <h1 className="text-3xl font-bold text-gray-50 mb-8">My Projects</h1>
             <div className="flex flex-col xl:flex-row gap-6">
-                <div className="flex-grow grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                    {projects.map(project => (
-                        <ProjectBox key={project.id} project={project} onDelete={onDelete} />
-                    ))}
-                </div>
-                {
-                    <ProjectForm onAddProject={handlePOSTRequest} />
-                }
+                <ProjectFlexbox projects={projects} onDelete={onDelete} />
+
+                <ProjectForm onAddProject={handlePOSTRequest} />
+
             </div>
         </div>
     )
