634f3f66b71280ea6d79bf4b02f81e4f21eddb1d
Niklas <niklasberby@gmail.com>
2024-09-25 20:55:46 +0200

    Refactor ProjectBox component to remove unnecessary styles and improve delete button alignment

--- frontend/src/components/ProjectBox.tsx
+++ frontend/src/components/ProjectBox.tsx
@@ -14,10 +14,10 @@ function ProjectBox({ project, onDelete }: ProjectBoxProps) {
     return (
         <div className="bg-zinc-800 shadow-lg rounded-lg p-6 mb-6">
 
-            <div className="flex justify-between items-center mb-4">
+            <div className="flex justify-between mb-4">
                 <h2 className="text-2xl font-bold text-gray-50 mb-2">{project.title}</h2>
                 <button
-                    className="w-6 h-6 bg-red-500 hover:bg-red-600 text-white rounded-full focus:outline-none focus:ring-2
+                    className="ml-4 w-6 h-6 min-w-6 bg-red-500 hover:bg-red-600 text-white rounded-full focus:outline-none focus:ring-2
                      focus:ring-red-500 focus:ring-opacity-50 transition-colors"
                     onClick={handleOnDelete}>
                 </button>
